2d353c84f0c5e061b43252350737b57d6b07c386
Fernando Bevilacqua <dovyski@gmail.com>
2021-07-29 11:48:49 -0300

    feat: melhoras no layout da página inicial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,65 +1,126 @@
 @extends('layouts.base')
 @section('content')
 
-<!-- ======= Hero Section ======= -->
-  <section id="hero" class="hero d-flex align-items-center">
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">Cendal de Serviços e Ideias</h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">Uma ponte entre você e a construção de uma universidade melhor</h2>
-          <div data-aos="fade-up" data-aos-delay="600">
-            <div class="text-center text-lg-start">
-              <a href="/servicos/solicitar" class="btn-get-started d-inline-flex align-items-center justify-content-center align-self-center">
-                  <span>Solicitar serviço</span>
-                  <i class="bi bi-arrow-right-circle"></i>
-              </a>
-              <a href="/feedbacks" class="btn-get-outline d-inline-flex align-items-center justify-content-center align-self-center">
-                  <span>Ideias</span>
-                  <i class="bi bi-lightbulb"></i>
-              </a>
+<section class="text-gray-600 body-font hero">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0" data-aos="zoom-out" data-aos-delay="200">
+            <img class="object-cover object-center w-full" alt="hero" src="{{ asset('img/manypixels.co/science-monochromatic.svg') }}">
+        </div>
+        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start text-left items-center">
+            <h1 data-aos="fade-up" class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Crie conteúdo digital
+            </h1>
+            <p data-aos="fade-up" data-aos-delay="400" class="mb-8 leading-relaxed">
+                Crie diversos materiais digitais de forma rápida e descomplicada. O melhor de tudo? Não precisa de conhecimento técnico algum! 
+                Deixe essa parte para o Maker fazer por você. Se preocúpe com o conteúdo, o resto é criado automaticamente.
+            </p>
+            <div data-aos="fade-up" data-aos-delay="600" class="flex justify-center">
+                <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
             </div>
-          </div>
         </div>
-        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
-          <img src="{{ asset('assets/img/hero-img.png') }}" class="img-fluid" alt="">
-        </div>
-      </div>
     </div>
+</section>
 
-  </section><!-- End Hero -->
-
-  <main id="main">
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-
-      <div class="container">
-        <div class="row gx-0">
-
-          <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos-delay="200">
-            <div class="content">
-              <h3>Boas-vindas!</h3>
-              <h2>Central de solicitação de serviços do PRACTICE e um local de discussão de ideias</h2>
-              <p>
-                Essa é a plataforma oficial do programa, onde a comunidade acadêmica pode solicitar e acompanhar serviços. Além disso, você pode dar sugestões de melhorias e ideias, fazendo parte do crescimento do programa e inovação da <a href="https://www.uffs.edu.br/"><strong>UFFS</strong></a>.
-              </p>
-              <div class="text-center text-lg-start">
-                <a href="https://practice.uffs.cc" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
-                  <span>Conheça o PRACTICE</span>
-                  <i class="bi bi-arrow-right"></i>
-                </a>
-              </div>
+<section class="text-gray-600 body-font">
+    <div class="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
+        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start text-left items-center">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Inteligência artificial
+                <br class="hidden lg:inline-block">ao seu dispor
+            </h1>
+            <p class="mb-8 leading-relaxed">
+                Nossa ferramenta utiliza tecnologias de inteligência artificial para entender o seu texto. A partir disso, criamos uma apresentação de acordo. 
+                Já imaginou escrever <em>"quero uma imagem para rede social de um evento dia 20, às 19 horas"</em> e ter como resultado um cartaz lindo?
+                Não imagine mais, isso é uma realidade aqui.
+            </p>
+            <div class="flex justify-center">
+                <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
             </div>
-          </div>
-
-          <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-            <img src="{{ asset('img/hero/estudio-practice.jpg') }}" class="img-fluid" alt="">
-          </div>
-
         </div>
-      </div>
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+            <img class="object-cover object-center rounded" alt="hero" src="{{ asset('img/manypixels.co/artificial-intelligence-monochromatic.svg') }}">
+        </div>
+    </div>
+</section>
 
-    </section><!-- End About Section -->
-  </main><!-- End #main -->
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-10 mx-auto">
+        <div class="text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
+            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+                Blue bottle crucifix vinyl
+                post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi
+                pug.
+            </p>
+            <div class="flex mt-6 justify-center">
+                <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+            </div>
+        </div>
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>
+                </div>
+                <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
+                    <p class="leading-relaxed text-base">
+                        Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                        taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.
+                    </p>
+                    <a class="mt-3 text-indigo-500 inline-flex items-center">
+                        Learn More
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                        <circle cx="6" cy="6" r="3"></circle>
+                        <circle cx="6" cy="18" r="3"></circle>
+                        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                </div>
+                <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
+                    <p class="leading-relaxed text-base">
+                        Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                        taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.
+                    </p>
+                    <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                </div>
+                <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
+                    <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                        taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+                    <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+    </div>
+</section>
+
 @endsection
