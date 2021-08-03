7569fb7b3279d774481a020b20141a6c6a26cf23
Fernando Bevilacqua <dovyski@gmail.com>
2021-08-03 12:00:45 -0300

    fix(user) erro de sintaxe em model

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,17 +66,6 @@ class User extends Authenticatable
         'profile_photo_url',
     ];
 
-    public function books() {
-        return $this->hasMany(Book::class);
-    }
-
-    public function arts() {
-        return $this->hasMany(Art::class);
-    }
-
-    public function sites() {
-        return $this->hasMany(Site::class);
-
     public static $crud = [
         'fields' => [
             'name' => [
@@ -105,7 +94,19 @@ class User extends Authenticatable
                 'list_column' => true
             ],
         ]
-    ];
+    ];    
+
+    public function books() {
+        return $this->hasMany(Book::class);
+    }
+
+    public function arts() {
+        return $this->hasMany(Art::class);
+    }
+
+    public function sites() {
+        return $this->hasMany(Site::class);
+    }
 
     public function isAdmin() {
         return $this->type == SELF::ADMIN;
