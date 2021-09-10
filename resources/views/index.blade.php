0b7b67375cba30eb5b9df7f0393954816d98e568
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2021-09-10 15:51:11 -0300

    Remove views/livewire inutilizadas, docs do google listados

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -136,7 +136,6 @@
                 </div>
             </div>
         </div>
-        <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
     </div>
 </section>
 
