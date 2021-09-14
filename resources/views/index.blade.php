9ea26965be62b5d3903abd275830cd5afb7f44b0
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2021-09-14 11:16:52 -0300

    Add poster na tela inicial/ Fix rota que volta do poster

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -98,18 +98,18 @@
             <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                 <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                        <circle cx="6" cy="6" r="3"></circle>
-                        <circle cx="6" cy="18" r="3"></circle>
-                        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
                     </svg>
                 </div>
                 <div class="flex-grow">
-                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Poster</h2>
                     <p class="leading-relaxed text-base">
-                        Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                        taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.
+                    Crie o material gráfico para publicações, lives, entre outros. 
+                    Aqui você encontra diversos modelos configuráveis 
+                    para agilizar seu dia-a-dia.    
                     </p>
-                    <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                    <a href="{{ route('poster')}}" class="mt-3 text-indigo-500 inline-flex items-center">Começar a criar
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
