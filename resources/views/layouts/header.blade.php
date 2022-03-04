fb3197cd71003897952d228f423b29d877166b68
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2022-03-04 15:35:33 -0300

    Fix: Use asset() to access assets

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="header fixed-top">
     <div class="container-lg d-flex align-items-center justify-content-between">
         <a href="{{ route('index') }}" class="logo d-flex align-items-center">
-            <img src="assets/img/practice-maker.png" alt="">
+            <img src="{{ asset('assets/img/practice-maker.png') }}" alt="">
             <span>{{ config('app.name') }}</span>
         </a>
 
